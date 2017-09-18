2719bea9936b68d5cfac181c9fd4a48e108c09f8
Petr Hosek <phosek@google.com>
2017-09-18 16:37:34 -0700

    Avoid using the prebuilt WebKit in WebView build
    Change-Id: I5c1385ba2027105d2b351bde39f974589c37198a

--- recipes/web_view.py
+++ recipes/web_view.py
@@ -60,6 +60,7 @@ def RunSteps(api, category, patch_gerrit_url, patch_project, patch_ref,
       api.path['start_dir'].join('packages/gn/gen.py'),
       '--target_cpu=%s' % gen_target,
       '--modules=webkit',
+      '--args', 'use_prebuilt_webkit=false',
       '--goma=%s' % api.goma.goma_dir,
       '--release',
     ])
