2d012dfcbb53443a3804967f9314aa91a3b17669
Joshua Seaton <joshuaseaton@google.com>
2018-08-23 09:35:24 -0700

    [botanist] Pin botanist version
    Pin botanist to version that does not use subcommands
    Bug: IN-555
    
    Change-Id: Ifc6f084d60db7e9ace8f6206c3871e7edd522bc6

--- recipes/zircon.py
+++ recipes/zircon.py
@@ -218,7 +218,7 @@ def RunTestsOnDevice(api, target, build_dir, device_type):
         io_timeout=TEST_IO_TIMEOUT_SECS,
         hard_timeout=40*60, # 40 minute hard timeout
         outputs=[output_archive_name],
-        cipd_packages=[('botanist', 'fuchsia/infra/botanist/linux-amd64', 'latest')],
+        cipd_packages=[('botanist', 'fuchsia/infra/botanist/linux-amd64', '390309732792e4a97a97385892f30c163ddd3e5e')],
     )
     # Collect results.
     results = api.swarming.collect(requests_json=api.json.input(trigger_result.json.output))
